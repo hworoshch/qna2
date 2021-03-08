deab0b268deaa9a7c7ff9136ff65505782677781
hworoshch <hworoshch@gmail.com>
2021-03-08 15:52:39 +0700

    Add answer (ajax)

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -12,11 +12,22 @@ RSpec.describe AnswersController, type: :controller do
         it 'saves a new answer in the database' do
           expect { post :create, params: { question_id: question, answer: attributes_for(:answer) }, format: :js }.to change(question.answers, :count).by(1)
         end
+
+        it 'redirects to show' do
+          post :create, params: { question_id: question, answer: attributes_for(:answer), format: :js }
+          # expect(response).to redirect_to question
+          expect(response).to render_template :create
+        end
       end
 
       context 'with invalid attributes' do
         it 'doesnt saves a new answer in the database' do
           expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }, format: :js }.not_to change(Answer, :count)
+        end
+
+        it 're-renders with errors' do
+          post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid), format: :js }
+          expect(response).to render_template :create
         end
       end
     end
