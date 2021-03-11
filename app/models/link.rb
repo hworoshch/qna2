e84e2ebff5d973a7405a960cd3248793384d2fb5
hworoshch <hworoshch@gmail.com>
2021-03-11 14:52:47 +0700

    gist detector + some refactoring

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,12 @@
 class Link < ApplicationRecord
+  GIST_URL = /^(https|http):\/\/gist\.github\.com\/([^\/]+)\/([a-f0-9]+)$/i
+
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
   validates :url, format: { with: URI.regexp }, if: Proc.new { |a| a.url.present? }
+
+  def gist_link?
+    url =~ GIST_URL
+  end
 end
