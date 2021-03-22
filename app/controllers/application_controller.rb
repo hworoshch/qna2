da3583249f30d34c46691a721b00e70e1c0a2d81
hworoshch <hworoshch@gmail.com>
2021-03-22 13:48:15 +0700

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,14 +8,13 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     @error = "You are not authorized to perform this action."
-    if request_format == :html
-      flash[:error] = @error
-      redirect_to request.headers["Referer"] || root_path
-    else
-      respond_to do |format|
-        format.json { render json: [@error], status: :forbidden }
-        format.js { render json: [@error], status: :forbidden }
+    respond_to do |format|
+      format.html do
+        flash[:error] = @error
+        redirect_to request.headers["Referer"] || root_path
       end
+      format.json { render json: [@error], status: :forbidden }
+      format.js { render json: [@error], status: :forbidden }
     end
   end
 end
