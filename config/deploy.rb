d725ea296d1bb70f10230e74e4df443a4157a112
hworoshch <hworoshch@gmail.com>
2021-04-01 06:41:13 +0700

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,11 @@ lock "~> 3.16.0"
 set :application, "qna"
 set :repo_url, "git@github.com:hworoshch/qna2.git"
 set :deploy_to, "/home/deployer/qna"
-set :deploy_user, 'deployer'
+set :deploy_user, "deployer"
 set :pty,  false
+
+set :init_system, :systemd
+set :service_unit_name, "sidekiq"
 
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
