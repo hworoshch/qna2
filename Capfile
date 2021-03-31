4cbed40c19276016271d29a18f37a02bc00068cc
hworoshch <hworoshch@gmail.com>
2021-04-01 01:07:19 +0700

    capistrano

---
+++ Capfile
@@ -0,0 +1,12 @@
+# Load DSL and set up stages
+require "capistrano/setup"
+require "capistrano/deploy"
+require "capistrano/rvm"
+require "capistrano/bundler"
+require "capistrano/rails"
+require "capistrano/passenger"
+
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "qna"
+set :repo_url, "git@github.com:hworoshch/qna2.git"
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+append :linked_files, "config/database.yml", "config/master.key"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
