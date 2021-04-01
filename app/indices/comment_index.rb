52b6e1990ba0db182fa60ea1a339cc147c741a4e
hworoshch <hworoshch@gmail.com>
2021-04-01 16:51:13 +0700

    fix

--- app/indices/comment_index.rb
+++ app/indices/comment_index.rb
@@ -2,5 +2,5 @@ ThinkingSphinx::Index.define :comment, with: :active_record do
   indexes body
   indexes user.email, as: :author, sortable: true
 
-  has user_id, created_at, updated_at
+  has user_id, commentable_type, created_at, updated_at
 end
