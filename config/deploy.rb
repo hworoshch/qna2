909489fdfcdf23f8406281b251d9c2ff89ccae4f
hworoshch <hworoshch@gmail.com>
2021-04-02 18:07:11 +0700

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,5 @@ set :deploy_user, "deployer"
 set :rvm_type, :user
 set :rvm_ruby_version, "2.7.2"
 
-set :init_system, :systemd
-set :service_unit_name, "sidekiq"
-
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
