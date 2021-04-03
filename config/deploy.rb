c9b624fab55f95740f7f4805ed970e648a73279a
hworoshch <hworoshch@gmail.com>
2021-04-03 18:04:58 +0700

    oauth

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :deploy_to, "/home/deployer/qna"
 set :deploy_user, "deployer"
 set :rvm_type, :user
 set :rvm_ruby_version, "2.7.2"
+set :init_system, :systemd
+set :service_unit_name, "sidekiq"
 
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
