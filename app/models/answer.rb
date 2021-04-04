7eb37c4397a7bf7ab34ec13b85809d8450f616c9
hworoshch <hworoshch@gmail.com>
2021-04-04 18:29:35 +0700

    caching

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ApplicationRecord
 
   has_many :links, dependent: :destroy, as: :linkable
   has_one :award, dependent: :destroy
-  belongs_to :question
+  belongs_to :question, touch: true
   belongs_to :user
 
   has_many_attached :files
